19efc84dba2ca8dd7a073827ee3de715cf265057
Tiago Gomes <tigoengenharia@gmail.com>
2025-11-24 09:43:45 -0300

    Ajuste elementos Modal adicionar tarefa

--- src/script/script.js
+++ src/script/script.js
@@ -29,15 +29,35 @@ function getInfoByAddForm() {
     const inputAtividade = document.getElementById('add-atividade').value;
     const inputDescricao = document.getElementById('add-descricao').value;
     const inputPrazo = document.getElementById('add-prazo').value;
+    const inputImportante = document.getElementById('add-importante').checked;
 
     //Armezar informações coletadas em um objeto
-
     const inputObj = {
         atividade: inputAtividade,
         descricao: inputDescricao,
-        prazo: inputPrazo
+        prazo: inputPrazo,
+        importante: inputImportante
 
     };
 
     console.log(inputObj);
+    addToLocalStorage(inputObj);
+    fecharModal();
+
 }
+
+/*
+Função armazenar dados localmente
+*/
+function addToLocalStorage(Obj) {
+    localStorage.setItem('tarefas', JSON.stringify(Obj));
+}
+/*
+Função responsável por resgatar e decodificar os dados da tarefa que foram salvos na memória local do navegador
+*/
+function getFromLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('tarefas'));
+    console.log(data);
+    return data;
+}
+getFromLocalStorage();
